3d3b055e119604581ad77f8655f93f0590a7df10
Shep Bryan IV <jsbryan4@asu.edu>
2023-04-23 10:48:02 -0700

    fixed typo

--- get_from_cluster.sh
+++ get_from_cluster.sh
@@ -9,17 +9,16 @@ password=$(head -n 1 _password_.txt)
 project=$1
 
 echo "Copying $project outfiles..."
-echo "$PROJECTSPATH$project/outfiles/."
 
-# directories=$(sshpass -p "$password" ssh "$my_name@agave.asu.edu" ls .)
+directories=$(sshpass -p "$password" ssh "$my_name@agave.asu.edu" ls .)
 
-# for directory in $directories
-# do
-#   if [[ $directory = $project* ]]
-#   then
-#     echo "$directory"
-#     path_agave="$my_name@agave.asu.edu:/home/$my_name/$directory/outfiles/*"
-#     path_local="$PROJECTSPATH$project/outfiles/."
-#     sshpass -p "$password" rsync -rv "$path_agave" "$path_local"
-#   fi
-# done
+for directory in $directories
+do
+  if [[ $directory = $project* ]]
+  then
+    echo "$directory"
+    path_agave="$my_name@agave.asu.edu:/home/$my_name/$directory/outfiles/*"
+    path_local="$PROJECTSPATH$project/outfiles/."
+    sshpass -p "$password" rsync -rv "$path_agave" "$path_local"
+  fi
+done
