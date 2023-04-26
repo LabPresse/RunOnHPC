a729160169eb548184ff262b56bccd9fae598042
Shep Bryan IV <jsbryan4@asu.edu>
2023-04-26 14:06:32 -0700

    Added comments

--- get_from_cluster.sh
+++ get_from_cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Load variables
 my_name=$(head -n 1 _username_.txt)
 password=$(head -n 1 _password_.txt)
 project=$1
@@ -10,12 +11,14 @@ else
   savepath=$2
 fi
 
+# Parse directories
 echo "Copying $project outfiles..."
-
 directories=$(sshpass -p "$password" ssh "$my_name@agave.asu.edu" ls .)
 
+# Loop through directories
 for directory in $directories
 do
+  # If directory is a project directory copy files to local
   if [[ $directory = $project* ]]
   then
     echo "$directory"
@@ -24,3 +27,4 @@ do
     sshpass -p "$password" rsync -rv "$path_agave" "$path_local"
   fi
 done
+echo "Done."
--- upload_data.sh
+++ upload_data.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# Set ENV variables
-DATAPATH=$(head -n 1 _datapath_.txt)
-PROJECTSPATH=$(head -n 1 _projectspath_.txt)
-
+# Get variables
 my_name=$(head -n 1 _username_.txt)
 password=$(head -n 1 _password_.txt)
-
 project=$1
-
 path_local="$DATAPATH$project"
 path_agave="$my_name@agave.asu.edu:/home/$my_name/Data/"
 
+# Copy data to cluster
 echo "Copying data from $path_local to $path_agave"
 sshpass -p "$password" rsync -av --exclude=.* --exclude=*/old --exclude=*/Raw --exclude=*/raw $path_local $path_agave
-
-echo "..done"
+echo "Done."
 
